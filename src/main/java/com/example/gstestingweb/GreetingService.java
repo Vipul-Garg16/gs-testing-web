5bbc76dcaccf83d785f222638d70f2736c8b44af
Vipul-Garg16 <vipulgarg016@gmai.com>
2023-10-08 17:22:38 +0530

    Initial Commit

---
+++ src/main/java/com/example/gstestingweb/GreetingService.java
@@ -0,0 +1,12 @@
+package com.example.gstestingweb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String greet(){
+        return "Namaste";
+    }
+
+}
